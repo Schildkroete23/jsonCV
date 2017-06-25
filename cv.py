29e4f9eef97aeeefb2f93e10b3d5726734c9c95f
Daniel Petró <daniel.petro@student.uni-siegen.de>
2017-06-25 16:55:18 +0200

    Add english cv option.

--- cv.py
+++ cv.py
@@ -10,7 +10,7 @@ with open(sys.argv[1], 'r') as jsonData:
 if sys.argv[-1].lower() == "pdf":
     # write data to tex file
     latex = "\\documentclass[11pt]{article}\n"
-    latex += "\\usepackage[ngerman]{babel}\n"
+    latex += "\\usepackage[ngerman, english]{babel}\n"
     latex += "\\usepackage{fontawesome}\n"
     # set main font type
     latex += "\\setmainfont{SOURCE SANS PRO}\n"
@@ -25,6 +25,8 @@ if sys.argv[-1].lower() == "pdf":
     latex += "{0ex}{}\n"
     latex += "\\titlespacing*{\\subsection}{0ex}{2ex}{0ex}\n"
     latex += "\\begin{document}%\n"
+    if data["language"] == "german":
+        latex += "\selectlanguage{ngerman}\n"
     # turn off page numbering
     latex += "\\pagenumbering{gobble}%\n"
     # general information
@@ -78,14 +80,19 @@ if sys.argv[-1].lower() == "pdf":
         latex += "\\\\\n\\faLink\\ \\href{{{0}}}{{{0}}}\n".format(
             data["homepage"])
     latex += "\\end{tabbing}%\n"
-    latex += "{\\raggedright{%\n"
     # personal data
     if (data.get("nationality", False)
             or data.get("birthday", False)
             or data.get("birthplace", False)):
-        latex += "\\subsection*{\\faInfo\\ Persönliche Daten}\n"
+        if data["language"] == "german":
+            latex += "\\subsection*{\\faInfo\\ Persönliche Daten}\n"
+        else:
+            latex += "\\subsection*{\\faInfo\\ Personal Data}\n"
     if data.get("nationality", False):
-        latex += "Staatsangehörigkeit:\\\\ {\\textit{"
+        if data["language"] == "german":
+            latex += "Staatsangehörigkeit:\\\\ {\\textit{"
+        else:
+            latex += "Nationality:\\\\ {\\textit{"
         counter = 0
         for nation in data["nationality"]:
             counter += 1
@@ -94,42 +101,105 @@ if sys.argv[-1].lower() == "pdf":
                 latex += ", "
         latex += "}}\\\\\n"
     if data.get("birthday", False):
-        latex += "Geburtsdatum:\\\\{{\\textit{{{}}}}}\\\\\n".format(
-            data["birthday"])
+        if data["language"] == "german":
+            latex += "Geburtsdatum:\\\\{{\\textit{{{}}}}}\\\\\n".format(
+                data["birthday"])
+        else:
+            latex += "Date of Birth:\\\\{{\\textit{{{}}}}}\\\\\n".format(
+                data["birthday"])
     if data.get("birthplace", False):
-        latex += "Geburtsort:\\\\{{\\textit{{{}}}}}\\\\\n".format(
-            data["birthplace"])
+        if data["language"] == "german":
+            latex += "Geburtsort:\\\\{{\\textit{{{}}}}}\n".format(
+                data["birthplace"])
+        else:
+            latex += "Place of Birth:\\\\{{\\textit{{{}}}}}\n".format(
+                data["birthplace"])
     # languages
     if data.get("languages", False):
-        latex += "\\subsection*{\\faGlobe\\ Sprachen}\n"
+        if data["language"] == "german":
+            latex += "\\subsection*{\\faGlobe\\ Sprachen}\n"
+        else:
+            latex += "\\subsection*{\\faGlobe\\ Languages}\n"
+        latex += "\\begin{flushleft}\n"
         for language in data["languages"]:
             latex += language
             if language != data["languages"][-1]:
                 latex += ", "
+        latex += "\n\\end{flushleft}"
     # coding skills
     if data.get("coding", False):
-        latex += "\n\\subsection*{\\faCode\\ Computerprachen}\n"
+        if data["language"] == "german":
+            latex += "\n\\subsection*{\\faCode\\ Computerprachen}\n"
+        else:
+            latex += "\n\\subsection*{\\faCode\\ Coding}\n"
+        latex += "\\begin{flushleft}\n"
         for item in data["coding"]:
             latex += item
             if item != data["coding"][-1]:
                 latex += ", "
+        latex += "\n\\end{flushleft}"
     # software skills
     if data.get("software", False):
         latex += "\n\\subsection*{\\faDesktop\\ Software}\n"
+        latex += "\\begin{flushleft}\n"
         for item in data["software"]:
             latex += item
             if item != data["software"][-1]:
                 latex += ", "
-    latex += "}}%\n"
-    latex += "\\vfill\n"
+        latex += "\n\\end{flushleft}"
+    latex += "\n\\vfill\n"
     # place and date for signature
     latex += "{}, \\today\\\\\n\\\\\n\\\\%\n}}%\n".format(data["city"])
     latex += "\\hfill\\vline\\hfill%\n"
     latex += "\parbox[t][.99\\textheight]{.7\\textwidth}{%\n"
     latex += "\\vspace{0pt}%\n"
+    # education
+    if data.get("education", False):
+        if data["language"] == "german":
+            latex += "\\section*{\\faGraduationCap\\ Bildung}\n"
+        else:
+            latex += "\\section*{\\faGraduationCap\\ Education}\n"
+        for i in range(len(data["education"])):
+            institute = data["education"][i]
+            latex += "\\subsection*{{{0}}}\n".format(institute["institution"])
+            latex += "\\faMapMarker\\ {}".format(institute["place"])
+            if institute.get("end", False):
+                latex += " \\ \\faCalendar\\ {} - {}".format(
+                    institute["begin"], institute["end"])
+            else:
+                latex += " \\ \\faCalendar\\ {}".format(institute["begin"])
+            if institute.get("role", False):
+                latex += "\\\\\n\\textit{{{0}}}".format(institute["role"])
+            if institute.get("graduation", False):
+                if data["language"] == "german":
+                    latex += "\\\\\n\\underline{Abschluss}: "
+                else:
+                    latex += "\\\\\n\\underline{Degree}: "
+                latex += institute["graduation"]
+            if institute.get("ba_topic", False):
+                if data["language"] == "german":
+                    latex += "\\\\\n\\underline{Thema der Bachelorarbeit}: "
+                else:
+                    latex += "\\\\\n\\underline{Topic of Bachelorthesis}: "
+                latex += institute["ba_topic"]
+            if institute.get("focus", False):
+                if data["language"] == "german":
+                    if institute.get("focus", False):
+                        latex += "\\\\\n\\underline{Schwerpunkt}: "
+                    else:
+                        latex += "\\\\\n\\underline{Schwerpunkte}: "
+                else:
+                    latex += "\\\\\n\\underline{Focus}: "
+                for item in institute["focus"]:
+                    latex += item
+                    if item != institute["focus"][-1]:
+                        latex += ", "
     # work experience
     if data.get("work", False):
-        latex += "\\section*{\\faGears\\ Arbeitserfahrung}\n"
+        if data["language"] == "german":
+            latex += "\\section*{\\faGears\\ Arbeitserfahrung}\n"
+        else:
+            latex += "\\section*{\\faGears\\ Work Experience}\n"
         for company in data["work"]:
             latex += "\\subsection*{{{0}}}\n".format(company["company"])
             latex += "\\faMapMarker\\ {}".format(company["place"])
@@ -149,38 +219,12 @@ if sys.argv[-1].lower() == "pdf":
                     if task != company["tasks"][-1]:
                         latex += "; "
             latex += "\n"
-    # education
-    if data.get("education", False):
-        latex += "\\section*{\\faGraduationCap\\ Bildung}\n"
-        for i in range(len(data["education"])):
-            institute = data["education"][i]
-            latex += "\\subsection*{{{0}}}\n".format(institute["institution"])
-            latex += "\\faMapMarker\\ {}".format(institute["place"])
-            if institute.get("end", False):
-                latex += " \\ \\faCalendar\\ {} - {}".format(
-                    institute["begin"], institute["end"])
-            else:
-                latex += " \\ \\faCalendar\\ {}".format(institute["begin"])
-            if institute.get("role", False):
-                latex += "\\\\\n\\textit{{{0}}}".format(institute["role"])
-            if institute.get("graduation", False):
-                latex += "\\\\\n\\underline{Abschluss}: "
-                latex += institute["graduation"]
-            if institute.get("ba_topic", False):
-                latex += "\\\\\n\\underline{Thema der Bachelorarbeit}: "
-                latex += institute["ba_topic"]
-            if institute.get("focus", False):
-                if institute.get("focus", False):
-                    latex += "\\\\\n\\underline{Schwerpunkt}: "
-                else:
-                    latex += "\\\\\n\\underline{Schwerpunkte}: "
-                for item in institute["focus"]:
-                    latex += item
-                    if item != institute["focus"][-1]:
-                        latex += ", "
     # civil service
     if data.get("civil_service", False):
-        latex += "\\section*{\\faGroup\\ Zivildienst}\n"
+        if data["language"] == "german":
+            latex += "\\section*{\\faGroup\\ Zivildienst}\n"
+        else:
+            latex += "\\section*{\\faGroup\\ Civil Service}\n"
         for company in data["civil_service"]:
             tmpWord = ""
             for letter in company["company"]:
